a0e925d39f2050fddda7e35af5faa249f7d8dd24
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-01-06 14:52:23 +0100

    Change a file's name

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -7,5 +7,3 @@ export const init = <A>(arr: A[]): A[] =>
   arr.length - 1 < 0 ? [] : arr.slice(0, arr.length - 1);
 export const lastUnsafe = <A>(arr: A[]): A => arr[arr.length - 1];
 export const isEmpty = <A>(arr: A[]): boolean => arr.length === 0;
-
-export const firstUnsafe = <A, B>(arr: [A, B]): A => arr[0];
